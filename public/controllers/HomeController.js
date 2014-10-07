294f66e6f85407943cb763d8b53789b5f3d58fd0
Teodor Tunev <teodortunev@gmail.com>
2014-10-07 15:18:29 +0300

    Change Fav Icon

--- public/controllers/HomeController.js
+++ public/controllers/HomeController.js
@@ -1,9 +1,5 @@
 ﻿'use strict';
 
-appMain.controller('HomeController', function ($scope, $location, ArticlesResource) {
-    ArticlesResource.getAll()
-        .then(function (response) {
-            $scope.articles = response;
-            console.log($scope.articles);
-        });
+appMain.controller('HomeController', function ($scope, $location) {
+
 });
