cdd5c5bc714fec60e392872ddd72b20bb58c45d6
Mitko-Kerezov <d.kerezov@gmail.com>
2014-10-10 00:54:28 +0300

    Delete button visible to admins and some bug fixing

--- public/controllers/AdByIdController.js
+++ public/controllers/AdByIdController.js
@@ -1,7 +1,15 @@
 'use strict';
 
-appMain.controller('AdByIdController', function HomeController($scope, adsData , $routeParams) {
+appMain.controller('AdByIdController', function HomeController($scope, adsData , $routeParams, AccountService) {
+    var deleteAd = function deleteAd() {
+        //TODO: Add service to delete ad
+        console.log($routeParams);
+    };
+
     adsData.byId($routeParams.id).then(function(data){
         $scope.ad = data;
     });
+
+    $scope.checkRole = AccountService.checkRole;
+    $scope.deleteAd = deleteAd;
 });
