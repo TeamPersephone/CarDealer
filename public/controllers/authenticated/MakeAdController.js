a3f279febcb7a7754cd1cd3cfd92184d0b899ba1
Teodor Tunev <teodortunev@gmail.com>
2014-10-10 00:03:30 +0300

    Make a dropdown for Create

--- public/controllers/authenticated/MakeAdController.js
+++ public/controllers/authenticated/MakeAdController.js
@@ -1,42 +1,15 @@
 'use strict';
 
 appMain.controller('MakeAdController', function MakeAdController($scope, $location, AccountService, adsData) {
-    if (AccountService.userData.isAuth === false ) {
+    if (AccountService.userData.isAuth === false) {
         $location.path("/");
     }
     $scope.successfulRegistration = false;
     $scope.formMessage = "";
-
-    adsData.getMakes().then(function(makes){
-    $scope.makes = makes;
-    //$scope.selected = "";
-    $scope.models = makes[0].Model;
-
-    $scope.selectedMake = '';
-    $scope.selectedModel = '';
-
-    $scope.makeAd = function makeAdFunction(ad) {
-
-        if (ad.make === ""){
-            $scope.formMessage = "You must provide the maker's name";
-            return;
-        }
-
-        if (ad.model === ""){
-            $scope.formMessage = "You must provide the model";
-            return;
-        }
-
-        ad.published = new Date();
-        ad.user = AccountService.userData.userId;
-
-        adsData.postAd(ad)
-            .then(function (response) {
-                $scope.successfulRegistration = true;
-                $scope.formMessage = "Ad created successfully, redirecting to home page...";
-                $location.path("/");
-            }, function (error) {
-                $scope.formMessage = error.message;
-            });
-    }});
+    $scope.change = function () {
+        $scope.models =  $scope.makes[$scope.selectedMake].model;
+    }
+    adsData.getMakes().then(function (makes) {
+        $scope.makes = makes;
+    });
 });
